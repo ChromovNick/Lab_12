0b3c33f11444503573de85adff51a40abb6629c8
Никита Хромов <chromovnick@gmail.com>
2022-01-10 21:49:01 +0300

    Добавьте файлы проекта.

---
+++ Lab_12/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    static class Circle
+    {
+        static public double GetCircumference (double r)
+        {
+            return r * 2 * Math.PI;
+        }
+        static public double  GetSquare (double r)
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+        static public bool GetCoordinates (double x, double y, double r)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) < r;
+        }
+
+    }
+
+}
---
+++ Lab_12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x0 = 0;
+            double y0 = 0;
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату х точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату у точки");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double circumference = Circle.GetCircumference(r);
+            double square = Circle.GetSquare(r);
+            bool coordinates = Circle.GetCoordinates (x, y, r);
+            Console.WriteLine("Длина окружности равна {0:f2}", circumference);
+            Console.WriteLine("Площадь окружности равна {0:f2}", square);
+            Console.WriteLine("Принадлежность точки: {0}", coordinates);
+            Console.ReadKey();
+        }
+    }
+}
